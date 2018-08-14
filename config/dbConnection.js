2fd36d0cf08eefd9a53cf9e577eb078ca8d275df
Daniel Patricio <daniel@Daniels-MacBook-Air.local>
2018-08-14 13:26:59 -0300

    db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,5 @@
 const { Client } = require('pg')
 const connectionString = 'postgresql://daniel:23032013@database.server.com:5433/snet'
-
 const client = new Client({
   connectionString: connectionString,
 })
