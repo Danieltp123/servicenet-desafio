e134c8056fdd328312b257db0d942feee7b45675
Daniel Patricio <daniel@Daniels-MacBook-Air.local>
2018-08-14 13:58:25 -0300

    dp

--- app.js
+++ app.js
@@ -3,5 +3,4 @@ var app = require('./config/server');
 app.listen(8080, function(){
     console.log('Servidor ON');
     
-    
 });
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,20 @@
-const { Client } = require('pg')
-const connectionString = 'postgresql://daniel:23032013@database.server.com:5432/snet'
-const client = new Client({
-  connectionString: connectionString,
-})
-client.connect()
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
-
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 module.exports =function(){
     return client;
 }
