30d00fc73f900d03497ae65665c1a94bf921a422
Daniel Patricio <danieltp123@hotmail.com>
2018-08-17 05:31:29 -0300

    Commit final

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,28 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
-  styleUrls: ['./read.component.css']
+  styleUrls: ['../../bootstrap.min.css','./read.component.css']
 })
 export class ReadComponent implements OnInit {
-  client:any;
-  _router: any;
+  
+  clientes:Observable<any[]>;
 
-  constructor(private angularFire: AngularFireDatabase,) { 
+  constructor(
+    private angularFire: AngularFireDatabase,
+    private _router: Router
+  ) { 
     var user = firebase.auth().currentUser;
     if (!user) {
      this._router.navigate(['/']);
     }
     
   }
-
+  updateItem(key: string, nome: string,fone: string) {
+    this.angularFire.list('client').update(key, { name: nome,phone: fone });
+  }
+  deleteItem(key: string) {    
+    this.angularFire.list('client').remove(key); 
+  }
   ngOnInit() {
-   this.client = this.angularFire.list('client');
-   console.log(this.client.name);
-   
-   }
+    this.clientes = this.angularFire.list('client').valueChanges();;
+    console.log(this.clientes);
+  }
  }
 
 
